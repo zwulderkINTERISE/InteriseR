cc0accd336516ca6e1e24f2691a9767303974f0b
zwulderkINTERISE <50119476+zwulderkINTERISE@users.noreply.github.com>
2021-01-23 09:11:20 -0600

    Initial commit

--- 2021.01.23 InteriseR.R
+++ 2021.01.23 InteriseR.R
@@ -1,4 +1,4 @@
-pacman::p_load(tidyverse, readxl, magrittr, janitor)
+pacman::p_load(tidyverse, readxl, magrittr, janitor, usethis, fs, devtools)
 options(stringsAsFactors = FALSE)
 
 create_package("C:/Users/zwulderk/Desktop/Work/InteriseR")
